76dfd96aa7833ef0bec383f66971b7ad79aafde8
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-29 01:11:22 +0200

    feat: create button component

---
+++ components/button.tsx
@@ -0,0 +1,35 @@
+import classNames from "classnames";
+import { PropsWithChildren } from "react";
+
+interface ButtonProperties {
+  type: "button" | "submit";
+  variant: "outline" | "solid";
+  className?: string;
+  onclick?: () => void;
+}
+
+export default function Button({
+  className,
+  type = "button",
+  variant = "solid",
+  children,
+  onclick,
+}: PropsWithChildren<Partial<ButtonProperties>>) {
+  return (
+    <button
+      onClick={onclick}
+      type={type}
+      className={classNames(
+        "border transition border-primary py-1 px-4",
+        className,
+        {
+          "bg-primary/100 hover:bg-primary/80 text-white": variant === "solid",
+          "text-primary hover:bg-primary hover:text-white":
+            variant === "outline",
+        },
+      )}
+    >
+      {children}
+    </button>
+  );
+}
