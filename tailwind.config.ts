ce678dcda3a5c9910469b911a7b7ff0c35f06eca
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-28 17:54:33 +0200

    feat: create color components in tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,18 @@ const config: Config = {
   content: ["./components/**/*.{ts,tsx,mdx}", "./app/**/*.{ts,tsx,mdx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#A27B5C",
+        secondary: "#3F4E4F",
+        tertiary: "#DCD7C9",
+        accent: {
+          yellow: "#FFA90B",
+          green: "#22D7A6",
+          red: "#F14C35",
+          blue: "#129BFF",
+          purple: "#5A3CF3",
+        },
+      },
       backgroundImage: {},
     },
   },
