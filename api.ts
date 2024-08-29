e70b607fe59fb5ce031470f24a6d4bcc0907e8ee
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-29 20:27:58 +0200

    feat: create ProductDto interface

---
+++ api.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+interface ProductDto {
+  id: string;
+  name: string;
+  price: number;
+  previousPrice?: number;
+  hasDiscountInApp?: boolean;
+  description: string;
+}
