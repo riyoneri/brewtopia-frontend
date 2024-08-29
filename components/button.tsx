9ee7ccec4cb0c4ce44ce9c901e05819fa59655ea
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-29 20:28:28 +0200

    style: add hover effect on hover component

--- components/button.tsx
+++ components/button.tsx
@@ -19,15 +19,11 @@ export default function Button({
     <button
       onClick={onclick}
       type={type}
-      className={classNames(
-        "border transition border-primary py-1 px-4",
-        className,
-        {
-          "bg-primary/100 hover:bg-primary/80 text-white": variant === "solid",
-          "text-primary hover:bg-primary hover:text-white":
-            variant === "outline",
-        },
-      )}
+      className={classNames("border transition py-1 px-4", className, {
+        "bg-primary border-primary hover:border-primary/0 hover:bg-primary/80 text-white":
+          variant === "solid",
+        "text-primary hover:bg-primary hover:text-white": variant === "outline",
+      })}
     >
       {children}
     </button>
