dceb54d58de3d4279c69aec9956e822ca1634a47
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-30 06:49:31 +0200

    feat: create WorkspaceDto

--- api.ts
+++ api.ts
@@ -1,9 +1,16 @@
 /* eslint-disable no-unused-vars */
 interface ProductDto {
   id: string;
+  imageUrl: string;
   name: string;
   price: number;
   previousPrice?: number;
   hasDiscountInApp?: boolean;
   description: string;
+}
+
+interface WorkspaceDto {
+  id: string;
+  name: string;
+  imageUrl: string;
 }
