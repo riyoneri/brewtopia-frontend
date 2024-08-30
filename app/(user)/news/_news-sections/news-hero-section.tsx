e4d54a51f9252c562cd64a2a679623597c3c2b28
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-30 16:38:24 +0200

    feat: create news-hero-section

---
+++ app/(user)/news/_news-sections/news-hero-section.tsx
@@ -0,0 +1,18 @@
+import NewsHeadingCard from "@/components/news/news-heading-card";
+import News from "@/data/news";
+
+export default function NewsHeroSection() {
+  return (
+    <section className="hero-height grid bg-tertiary pb-5">
+      <div className="maximum-width flex flex-col justify-between  text-center">
+        <div>
+          <h1 className="main-heading">Our News</h1>
+          <p>
+            Get the lates updates and deeper coffee experience from BrewTopia
+          </p>
+        </div>
+        <NewsHeadingCard {...News[0]} className="flex-1 bg-red-500" />
+      </div>
+    </section>
+  );
+}
---
+++ app/(user)/news/page.tsx
@@ -0,0 +1,9 @@
+import NewsHeroSection from "./_news-sections/news-hero-section";
+
+export default function NewsPage() {
+  return (
+    <main>
+      <NewsHeroSection />
+    </main>
+  );
+}
