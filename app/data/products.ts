1300b45e4be6a4ffb2dcf780a794d506aa1e4f53
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-30 06:48:40 +0200

    feat: create products dummy data

---
+++ app/data/products.ts
@@ -0,0 +1,57 @@
+import { faker } from "@faker-js/faker";
+
+const products: ProductDto[] = [
+  {
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    imageUrl: faker.image.url(),
+    name: faker.commerce.productName(),
+    price: +faker.commerce.price({ min: 1, max: 10 }),
+  },
+  {
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    imageUrl: faker.image.url(),
+    name: faker.commerce.productName(),
+    hasDiscountInApp: true,
+    price: +faker.commerce.price({ min: 1, max: 10 }),
+  },
+  {
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    imageUrl: faker.image.url(),
+    name: faker.commerce.productName(),
+    previousPrice: +faker.commerce.price({ min: 10, max: 20 }),
+    price: +faker.commerce.price({ min: 1, max: 10 }),
+  },
+  {
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    imageUrl: faker.image.url(),
+    name: faker.commerce.productName(),
+    price: +faker.commerce.price({ min: 1, max: 10 }),
+  },
+  {
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    imageUrl: faker.image.url(),
+    name: faker.commerce.productName(),
+    price: +faker.commerce.price({ min: 1, max: 10 }),
+  },
+  {
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    imageUrl: faker.image.url(),
+    name: faker.commerce.productName(),
+    price: +faker.commerce.price({ min: 1, max: 10 }),
+  },
+  {
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    imageUrl: faker.image.url(),
+    name: faker.commerce.productName(),
+    price: +faker.commerce.price({ min: 1, max: 10 }),
+  },
+];
+
+export default products;
