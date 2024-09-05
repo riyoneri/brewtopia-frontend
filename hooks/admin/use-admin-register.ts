723c969bf75f8878465ca0a4e2b848a8d68993a8
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-05 07:10:17 +0200

    feat: correctly type admin registration custom hook

--- hooks/admin/use-admin-register.ts
+++ hooks/admin/use-admin-register.ts
@@ -1,8 +1,12 @@
 import { fetcher } from "@/helpers/fetcher";
 import { useMutation } from "@tanstack/react-query";
 
-export default function useRegisterAdmin() {
-  const { isPending, mutate, error, data } = useMutation({
+export default function useRegisterAdmin<T>() {
+  const { isPending, mutate, error, data } = useMutation<
+    AdminDto,
+    GlobalResponseError<T>,
+    string
+  >({
     mutationFn: (body: string) =>
       fetcher({ url: "/admin/auth/register", body, method: "POST" }),
   });
