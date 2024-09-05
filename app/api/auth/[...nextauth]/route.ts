7e69e8e31c896f29f727fccaa2131bcabf942bbc
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-05 20:35:58 +0200

    feat: create next auth route endpoint

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {};
+
+const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/admin/auth/google`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user),
+          },
+        );
+
+        if (!response.ok) return false;
+
+        return true;
+      } catch {
+        return "/obsidian/auth/login";
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
