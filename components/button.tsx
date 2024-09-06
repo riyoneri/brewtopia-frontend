9616176c4e87c09d4f925711f070cb4687e278cb
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-06 15:11:14 +0200

    feat: add disabled property on button component

--- components/button.tsx
+++ components/button.tsx
@@ -5,6 +5,7 @@ interface ButtonProperties {
   type: "button" | "submit";
   variant: "outline" | "solid";
   className?: string;
+  disabled?: boolean;
   onclick?: () => void;
 }
 
@@ -13,11 +14,13 @@ export default function Button({
   type = "button",
   variant = "solid",
   children,
+  disabled,
   onclick,
 }: PropsWithChildren<Partial<ButtonProperties>>) {
   return (
     <button
       onClick={onclick}
+      disabled={disabled}
       type={type}
       className={classNames("border transition py-1 px-4", className, {
         "bg-primary border-primary hover:border-primary/0 hover:bg-primary/80 text-white":
