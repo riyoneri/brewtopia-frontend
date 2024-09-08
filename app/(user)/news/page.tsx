b82f4bb49dda7adbee300d633dca6eec9b318608
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-08 12:56:49 +0200

    feat: delete hero news section

--- app/(user)/news/page.tsx
+++ app/(user)/news/page.tsx
@@ -1,13 +1,24 @@
+import NewsHeadingCard from "@/components/news/news-heading-card";
 import NewsList from "@/components/news/news-list";
-
-import NewsHeroSection from "./_news-sections/news-hero-section";
+import News from "@/data/news";
 
 export default function NewsPage() {
   return (
     <>
       <title>News</title>
       <main className="space-y-10">
-        <NewsHeroSection />
+        <section className="dynamic-hero-height grid bg-tertiary pb-5">
+          <div className="maximum-width flex flex-col justify-between  text-center">
+            <div>
+              <h1 className="main-heading">Our News</h1>
+              <p>
+                Get the lates updates and deeper coffee experience from
+                BrewTopia
+              </p>
+            </div>
+            <NewsHeadingCard {...News[0]} className="flex-1" />
+          </div>
+        </section>
         <NewsList />
       </main>
     </>
