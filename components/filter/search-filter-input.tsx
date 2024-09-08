a874f4a5334dd1883c836e2ed57f1b531039cd5f
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-09 00:12:41 +0200

    feat: create searc-filter input component

---
+++ components/filter/search-filter-input.tsx
@@ -0,0 +1,20 @@
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+
+export default function SearchFilterInput() {
+  return (
+    <div className="flex-1">
+      <div className="flex items-center justify-between">
+        <span>Search</span>
+        <button>Clear</button>
+      </div>
+      <label className="flex items-center gap-2 border-2 border-secondary/50 p-1">
+        <HiOutlineMagnifyingGlass />
+        <input
+          type="text"
+          placeholder="Keyword"
+          className="flex-1 outline-none"
+        />
+      </label>
+    </div>
+  );
+}
