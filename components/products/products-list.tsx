c45c76199cb1f1b36bfde42652ebb694b479f925
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-10 16:11:58 +0200

    feat: create product list component

---
+++ components/products/products-list.tsx
@@ -0,0 +1,45 @@
+import Button from "../button";
+import ProductCard from "./product-card";
+import ProductCardSkeleton from "./product-card-skeleton";
+
+interface ProductsListProperties {
+  products?: ProductDto[];
+  error?: string;
+  loading?: boolean;
+}
+
+export default function ProductsList({
+  error,
+  loading,
+  products,
+}: ProductsListProperties) {
+  return (
+    <>
+      <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        {error && (
+          <p className="col-span-full text-center text-red-500">{error}</p>
+        )}
+        {loading && (
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        )}
+        {products &&
+          (products?.length ? (
+            products.map((product) => (
+              <ProductCard {...product} key={product.id} />
+            ))
+          ) : (
+            <p className="col-span-full mt-5 sm:text-center sm:text-xl">
+              No Products available.
+            </p>
+          ))}
+      </div>
+      <Button variant="outline" className="mx-auto block w-full xs:w-fit">
+        Load more
+      </Button>
+    </>
+  );
+}
