59ddba4ef3b649af59e11ac583990e1cb9edbfb9
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-13 10:49:59 +0200

    fix: add mount check of categories with products component

--- components/categories-with-products.tsx
+++ components/categories-with-products.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import Products from "@/data/products";
+import { useEffect, useState } from "react";
+
 import CategoriesList from "./categories/categories-list";
 import ProductsList from "./products/products-list";
 
 export default function CategoriesWithProducts() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  if (!isMounted) return;
   return (
     <>
       <CategoriesList loading />
 
-      <ProductsList loading />
+      <ProductsList products={Products} />
     </>
   );
 }
