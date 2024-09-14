ce52ab173612a0961dccba9266d5e783acae8130
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-14 23:43:56 +0200

    feat: create cart items dummy data

---
+++ data/cart-items.ts
@@ -0,0 +1,110 @@
+import { faker } from "@faker-js/faker";
+
+const CartItems: CartItemDto[] = [
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    product: {
+      id: faker.string.uuid(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.url(),
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price({ min: 1, max: 10 }),
+      quantity: 0,
+    },
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    totalPrice: faker.number.int({ min: 50, max: 150 }),
+  },
+];
+
+export default CartItems;
