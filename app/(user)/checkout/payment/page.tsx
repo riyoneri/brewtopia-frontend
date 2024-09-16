e2f30c5846a5b4c04907407dc1edce02796f216c
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-16 07:17:10 +0200

    feat: implement initial steps of checkout/payment page

---
+++ app/(user)/checkout/payment/page.tsx
@@ -0,0 +1,17 @@
+export default function CheckoutPaymentPage() {
+  return (
+    <div>
+      <div className="border-b">
+        <div>
+          <span>Contact</span>
+          <span>Lion@email.com</span>
+        </div>
+        <div>
+          <span>Ship to</span>
+          <span>Lion@email.com</span>
+        </div>
+      </div>
+      <h3>Payment method</h3>
+    </div>
+  );
+}
