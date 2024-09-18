7f303068a55a5f066c42f9b14baf6da99e44d35d
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-18 09:08:40 +0200

    feat: implement first steps of order items list component

---
+++ components/order/order-items-list.tsx
@@ -0,0 +1,10 @@
+export default function OrderItemsList() {
+  return (
+    <div>
+      <h3 className="text-lg font-medium sm:text-2xl md:text-3xl">
+        Your Order
+      </h3>
+      <p>List items</p>
+    </div>
+  );
+}
