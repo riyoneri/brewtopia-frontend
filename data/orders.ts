5597782cec8f686e6a4e7cf752a7e6361b4ad888
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-18 17:46:35 +0200

    feat: create orders dummny data

---
+++ data/orders.ts
@@ -0,0 +1,87 @@
+import { faker } from "@faker-js/faker";
+import dayjs from "dayjs";
+
+import CartItems from "./cart-items";
+
+const minimumDate = dayjs().subtract(3, "months").toISOString();
+const maximumDate = dayjs().add(7, "months").toISOString();
+
+const Orders: OrderDto[] = [
+  {
+    id: faker.string.uuid(),
+    createdAt: faker.date
+      .between({ from: minimumDate, to: maximumDate })
+      .toISOString(),
+    paymentMethod: faker.helpers.arrayElement([
+      "Credit or Debit Card",
+      "MTN Mobile Money",
+    ]),
+    products: faker.helpers.arrayElements(CartItems, {
+      min: 3,
+      max: CartItems.length - 1,
+    }),
+    total: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    createdAt: faker.date
+      .between({ from: minimumDate, to: maximumDate })
+      .toISOString(),
+    paymentMethod: faker.helpers.arrayElement([
+      "Credit or Debit Card",
+      "MTN Mobile Money",
+    ]),
+    products: faker.helpers.arrayElements(CartItems, {
+      min: 3,
+      max: CartItems.length - 1,
+    }),
+    total: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    createdAt: faker.date
+      .between({ from: minimumDate, to: maximumDate })
+      .toISOString(),
+    paymentMethod: faker.helpers.arrayElement([
+      "Credit or Debit Card",
+      "MTN Mobile Money",
+    ]),
+    products: faker.helpers.arrayElements(CartItems, {
+      min: 3,
+      max: CartItems.length - 1,
+    }),
+    total: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    createdAt: faker.date
+      .between({ from: minimumDate, to: maximumDate })
+      .toISOString(),
+    paymentMethod: faker.helpers.arrayElement([
+      "Credit or Debit Card",
+      "MTN Mobile Money",
+    ]),
+    products: faker.helpers.arrayElements(CartItems, {
+      min: 3,
+      max: CartItems.length - 1,
+    }),
+    total: faker.number.int({ min: 50, max: 150 }),
+  },
+  {
+    id: faker.string.uuid(),
+    createdAt: faker.date
+      .between({ from: minimumDate, to: maximumDate })
+      .toISOString(),
+    paymentMethod: faker.helpers.arrayElement([
+      "Credit or Debit Card",
+      "MTN Mobile Money",
+    ]),
+    products: faker.helpers.arrayElements(CartItems, {
+      min: 3,
+      max: CartItems.length - 1,
+    }),
+    total: faker.number.int({ min: 50, max: 150 }),
+  },
+];
+
+export default Orders;
