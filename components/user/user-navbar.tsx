4370fd54470a3520fdb43d4b3484526f9bab4ac9
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-19 10:34:05 +0200

    feat: create additional links on navbar

--- components/user/user-navbar.tsx
+++ components/user/user-navbar.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
 import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
+import { FaCircleUser, FaLocationDot } from "react-icons/fa6";
 import { GoHome } from "react-icons/go";
+import { GrDeliver } from "react-icons/gr";
+import { IoMdSettings } from "react-icons/io";
 import { IoCloseOutline, IoNewspaperOutline } from "react-icons/io5";
 import { MdEvent, MdMenu, MdOutlineRestaurantMenu } from "react-icons/md";
 
@@ -33,9 +46,28 @@ const NavLinks = [
   },
 ];
 
+const DropDownNavLinks = [
+  {
+    text: "Orders",
+    icon: GrDeliver,
+    url: "/orders",
+  },
+  {
+    text: "Address List",
+    icon: FaLocationDot,
+    url: "/addresses",
+  },
+  {
+    text: "Account Settings",
+    icon: IoMdSettings,
+    url: "/account",
+  },
+];
+
 export default function UserNavBar() {
   const pathname = usePathname();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isAuth] = useState(true);
 
   const closeDrawer = () => setIsDrawerOpen(false);
 
@@ -67,9 +99,48 @@ export default function UserNavBar() {
             <Link href="/cart">
               <Button>Cart (0)</Button>
             </Link>
-            <Link href="/news">
-              <Button variant="outline">Sign In</Button>
-            </Link>
+            {!isAuth && (
+              <Link href="/auth/login">
+                <Button variant="outline">Sign In</Button>
+              </Link>
+            )}
+            {isAuth && (
+              <Menu>
+                <MenuButton className="box-border flex self-stretch border-2 border-primary/50 p-1 data-[open]:bg-primary data-[focus]:outline-1 data-[focus]:outline-white">
+                  {({ open }) => (
+                    <FaCircleUser
+                      className={classNames("text-2xl transition", {
+                        "text-primary": !open,
+                        "text-white": open,
+                      })}
+                    />
+                  )}
+                </MenuButton>
+
+                <MenuItems
+                  transition
+                  anchor="bottom end"
+                  className="z-50 mt-2 w-52 origin-top space-y-1 bg-primary p-2 text-start text-sm/6 text-white transition duration-100 ease-out *:block *:px-1 *:py-1.5 *:transition data-[closed]:scale-95 data-[closed]:opacity-0"
+                >
+                  {DropDownNavLinks.map((navLink) => (
+                    <MenuItem key={navLink.text}>
+                      <Link
+                        href={navLink.url}
+                        className="data-[focus]:bg-white/30"
+                      >
+                        {navLink.text}
+                      </Link>
+                    </MenuItem>
+                  ))}
+
+                  <MenuItem>
+                    <button className="block w-full bg-accent-red/40 text-start data-[focus]:bg-white/30">
+                      Sign Out
+                    </button>
+                  </MenuItem>
+                </MenuItems>
+              </Menu>
+            )}
           </div>
         </div>
       </nav>
@@ -96,7 +167,7 @@ export default function UserNavBar() {
           </div>
           <div className="dui-drawer-side">
             <div onClick={closeDrawer} className="dui-drawer-overlay"></div>
-            <div className="flex min-h-full w-5/6 flex-col bg-white py-3 xs:w-80 ">
+            <div className="flex min-h-full w-5/6 flex-col space-y-1 bg-white py-3 xs:w-80 ">
               <div className="flex items-center justify-between border-b px-2 pb-1">
                 <div className="font-medium">
                   <span>Brew</span>
@@ -122,6 +193,39 @@ export default function UserNavBar() {
                   </Link>
                 ))}
               </div>
+              <Disclosure as="div" className="flex flex-col" defaultOpen={true}>
+                <DisclosureButton className="group flex w-full items-center justify-between px-2 py-1 transition data-[open]:bg-tertiary hover:bg-tertiary">
+                  <div className="flex items-center gap-3">
+                    <FaCircleUser className="text-lg" />
+                    <span>Profile</span>
+                  </div>
+                  <FaChevronDown className="text-lg transition group-data-[open]:rotate-180" />
+                </DisclosureButton>
+                <DisclosurePanel
+                  transition
+                  className="origin-top space-y-1 py-2 text-sm/5 duration-200 ease-out data-[closed]:-translate-y-6 data-[open]:bg-tertiary/50 data-[closed]:opacity-0"
+                >
+                  {DropDownNavLinks.map((navLinks) => (
+                    <Link
+                      key={navLinks.text}
+                      href={navLinks.url}
+                      onClick={closeDrawer}
+                      className={classNames(
+                        "hover:bg-tertiary flex gap-2 py-1 pl-5 items-center transition",
+                        {
+                          "bg-tertiary": pathname === navLinks.url,
+                        },
+                      )}
+                    >
+                      <navLinks.icon className="text-lg" />
+                      {navLinks.text}
+                    </Link>
+                  ))}
+                </DisclosurePanel>
+              </Disclosure>
+              <button className="block w-full bg-accent-red/20 px-2 py-1 text-start transition hover:bg-neutral-200">
+                Sign Out
+              </button>
             </div>
           </div>
         </div>
