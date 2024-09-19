1160501603b8ab3ed0e99b69bf4a0b4f9b7796b7
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-19 13:33:20 +0200

    style: change colors on mobile navigation drawer

--- components/user/user-navbar.tsx
+++ components/user/user-navbar.tsx
@@ -17,7 +17,7 @@ import { FaChevronDown } from "react-icons/fa";
 import { FaCircleUser, FaLocationDot } from "react-icons/fa6";
 import { GoHome } from "react-icons/go";
 import { GrDeliver } from "react-icons/gr";
-import { IoMdSettings } from "react-icons/io";
+import { IoIosLogOut, IoMdSettings } from "react-icons/io";
 import { IoCloseOutline, IoNewspaperOutline } from "react-icons/io5";
 import { MdEvent, MdMenu, MdOutlineRestaurantMenu } from "react-icons/md";
 
@@ -167,7 +167,7 @@ export default function UserNavBar() {
           </div>
           <div className="dui-drawer-side">
             <div onClick={closeDrawer} className="dui-drawer-overlay"></div>
-            <div className="flex min-h-full w-5/6 flex-col space-y-1 bg-white py-3 xs:w-80 ">
+            <div className="flex min-h-full w-5/6 flex-col bg-white py-3 xs:w-80 ">
               <div className="flex items-center justify-between border-b px-2 pb-1">
                 <div className="font-medium">
                   <span>Brew</span>
@@ -184,9 +184,12 @@ export default function UserNavBar() {
                     key={navLink.text}
                     href={navLink.url}
                     onClick={closeDrawer}
-                    className={classNames("hover:bg-tertiary transition", {
-                      "bg-tertiary": pathname === navLink.url,
-                    })}
+                    className={classNames(
+                      "hover:bg-primary hover:text-white transition",
+                      {
+                        "bg-primary text-white": pathname === navLink.url,
+                      },
+                    )}
                   >
                     <NavIcon className="text-2xl" />
                     {navLink.text}
@@ -194,16 +197,16 @@ export default function UserNavBar() {
                 ))}
               </div>
               <Disclosure as="div" className="flex flex-col">
-                <DisclosureButton className="group flex w-full items-center justify-between px-2 py-1 transition data-[open]:bg-tertiary hover:bg-tertiary">
+                <DisclosureButton className="group flex w-full items-center justify-between px-2 py-1.5 transition data-[open]:bg-primary/70 hover:bg-primary hover:text-white">
                   <div className="flex items-center gap-3">
                     <FaCircleUser className="text-2xl" />
                     <span>Profile</span>
                   </div>
-                  <FaChevronDown className="text-lg transition group-data-[open]:rotate-180" />
+                  <FaChevronDown className="text-lg transition-transform group-data-[open]:rotate-180" />
                 </DisclosureButton>
                 <DisclosurePanel
                   transition
-                  className="origin-top space-y-1 py-2 text-sm/5 duration-200 ease-out data-[closed]:-translate-y-6 data-[open]:bg-tertiary/50 data-[closed]:opacity-0"
+                  className="origin-top space-y-1 py-2 text-sm/5 duration-200 ease-out data-[closed]:-translate-y-6 data-[open]:bg-primary/50 data-[closed]:opacity-0"
                 >
                   {DropDownNavLinks.map((navLinks) => (
                     <Link
@@ -211,9 +214,9 @@ export default function UserNavBar() {
                       href={navLinks.url}
                       onClick={closeDrawer}
                       className={classNames(
-                        "hover:bg-tertiary flex gap-2 py-1 pl-5 items-center transition",
+                        "hover:bg-primary hover:text-white flex gap-2 py-1 pl-5 items-center transition",
                         {
-                          "bg-tertiary": pathname === navLinks.url,
+                          "bg-primary": pathname === navLinks.url,
                         },
                       )}
                     >
@@ -223,7 +226,8 @@ export default function UserNavBar() {
                   ))}
                 </DisclosurePanel>
               </Disclosure>
-              <button className="block w-full bg-accent-red/20 px-2 py-1 text-start transition hover:bg-neutral-200">
+              <button className="mt-1 flex w-full items-center gap-3 bg-primary/20 px-2 py-1 text-start transition hover:bg-primary hover:text-white">
+                <IoIosLogOut className="text-2xl" />
                 Sign Out
               </button>
             </div>
