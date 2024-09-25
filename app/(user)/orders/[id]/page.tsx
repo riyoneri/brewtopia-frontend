19f12e28d47f67f3efa382abee942e0dbd6fccc3
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-25 17:32:22 +0200

    refactor: add back link button on order detials page

--- app/(user)/orders/[id]/page.tsx
+++ app/(user)/orders/[id]/page.tsx
@@ -1,7 +1,9 @@
+import Button from "@/components/button";
 import OrderProductsList from "@/components/order/order-products-list";
 import Orders from "@/data/orders";
 import dayjs from "dayjs";
-import { FaCircleCheck } from "react-icons/fa6";
+import Link from "next/link";
+import { FaArrowLeft, FaCircleCheck } from "react-icons/fa6";
 
 const order = Orders[0];
 
@@ -10,7 +12,7 @@ export default function OrderDetails() {
     <>
       <title>Order Details</title>
       <div className="from-white from-90% to-tertiary to-90% lg:bg-gradient-to-r ">
-        <div className="maximum-width flex flex-col gap-5 *:pt-5 lg:flex-row lg:gap-10">
+        <div className="maximum-width flex flex-col gap-5 *:py-5 lg:flex-row lg:gap-10">
           <div className="flex-1 space-y-5 bg-white">
             <h1 className="text-4xl font-medium">Order Details</h1>
             <div className="mt-10 space-y-2">
@@ -31,6 +33,12 @@ export default function OrderDetails() {
             </div>
             <hr className="border-2" />
             <OrderProductsList products={order.products} />
+            <Link href="/orders">
+              <Button variant="outline" className="flex items-center gap-2">
+                <FaArrowLeft />
+                Back
+              </Button>
+            </Link>
           </div>
           <div className="lg:dynamic-hero-height dui-timeline-compact mb-5 w-full max-w-full space-y-2 overflow-x-auto bg-tertiary pb-5 lg:mb-0 lg:w-96">
             <h3 className="sticky left-0 px-5 text-xl font-medium xs:text-2xl">
