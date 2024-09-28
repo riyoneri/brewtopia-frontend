ca97c4b3d2365b23ee52f25f3032ea9e480e44af
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-28 16:05:18 +0200

    feat: control select input in more convenient way

--- components/input-labels/select-input-label.tsx
+++ components/input-labels/select-input-label.tsx
@@ -1,45 +1,83 @@
+"use client";
+
 import { Select, SelectItem, SelectSection } from "@nextui-org/react";
 import classNames from "classnames";
+import { useState } from "react";
 import { Controller } from "react-hook-form";
+import { FaChevronDown } from "react-icons/fa6";
 
-interface SelectInputLabelProperties {
-  name: string;
-  selections: { key: string | number; text: string }[];
-  className?: string;
+interface SelectInputLabelProperties
+  extends Omit<FilterSortInputProperties, "clearHandler"> {
+  resetValue: () => void;
 }
 
 export default function SelectInputLabel({
+  title,
   name,
-  selections,
+  hasHeader = false,
+  selectOptions,
   className,
+  resetValue,
 }: SelectInputLabelProperties) {
+  const defaultOptionKey = selectOptions[0].key;
+  const [value, setValue] = useState(defaultOptionKey);
+
   return (
-    <Controller
-      name={name}
-      render={({ field }) => (
-        <Select
-          {...field}
-          aria-label="Select an number of orders"
-          className={classNames(className, "outline-none")}
-          size="sm"
-          radius="none"
-          variant="bordered"
-          defaultSelectedKeys={[`${selections[0].key}`]}
-          classNames={{
-            popoverContent: "rounded-none",
-            trigger:
-              "data-[open]:border-neutral-400 data-[focus]:border-neutral-400",
-          }}
-        >
-          <SelectSection>
-            {selections.map((selection) => (
-              <SelectItem className="rounded-none" key={selection.key}>
-                {selection.text}
-              </SelectItem>
-            ))}
-          </SelectSection>
-        </Select>
+    <label
+      htmlFor={name}
+      className={classNames(className, "space-y-2 sm:space-y-1 min-w-40")}
+    >
+      {hasHeader && (
+        <div className="flex items-center justify-between">
+          <span>{title}</span>
+          <button
+            onClick={() => {
+              setValue(defaultOptionKey);
+              resetValue();
+            }}
+          >
+            Clear
+          </button>
+        </div>
       )}
-    />
+
+      <Controller
+        name={name}
+        defaultValue={defaultOptionKey}
+        render={({ field: { onChange, ...otherFieldProperties } }) => (
+          <Select
+            {...otherFieldProperties}
+            aria-label="Select an number of orders"
+            className="min-w-max"
+            radius="none"
+            variant="bordered"
+            disabledKeys={[defaultOptionKey]}
+            selectedKeys={[value]}
+            value={value}
+            disallowEmptySelection
+            classNames={{
+              popoverContent: "rounded-none border-2 mt-3 border-neutral-300",
+              trigger:
+                "data-[open]:border-neutral-400 border-2 px-2 py-1 h-full data-[focus]:border-neutral-400 border-neutral-400 min-h-max",
+              selectorIcon: "relative size-10 end-0",
+              value: "text-sm sm:text-base leading-[1.33]",
+            }}
+            onChange={(event) => {
+              onChange(event);
+              setValue(event.target.value);
+            }}
+            selectorIcon={<FaChevronDown />}
+          >
+            <SelectSection>
+              {selectOptions.map((selectOption) => (
+                <SelectItem className="rounded-none" key={selectOption.key}>
+                  {selectOption.text}
+                </SelectItem>
+              ))}
+            </SelectSection>
+          </Select>
+        )}
+      />
+    </label>
   );
 }
