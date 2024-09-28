0b1588e48ffa5d2c7bbc41b93204b60fe117a189
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-28 16:20:36 +0200

    feat: create searc input label component

---
+++ components/input-labels/search-input-label.tsx
@@ -0,0 +1,40 @@
+import { AllInputsProperties } from "@/utils/input-properties";
+import classNames from "classnames";
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+
+interface SearchInputLabelProperties
+  extends Omit<FilterSortInputProperties, "selectOptions">,
+    Pick<AllInputsProperties, "register"> {}
+
+export default function SearchInputLabel({
+  className,
+  name,
+  clearHandler,
+  hasHeader = false,
+  title = "Search",
+  register,
+}: SearchInputLabelProperties) {
+  return (
+    <div className={classNames(className, "space-y-2 sm:space-y-1")}>
+      {hasHeader && (
+        <div className="flex items-center justify-between">
+          <span className="cursor-default">{title}</span>
+          <button onClick={clearHandler}>Clear</button>
+        </div>
+      )}
+      <label
+        htmlFor={name}
+        className="flex items-center gap-2 border-2 border-secondary/50 p-1"
+      >
+        <HiOutlineMagnifyingGlass />
+        <input
+          type="text"
+          id={name}
+          {...register}
+          placeholder="Keyword"
+          className="flex-1 outline-none"
+        />
+      </label>
+    </div>
+  );
+}
