88899e8aee3211df546f5394aef2c489cf2fe2c3
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-30 07:34:18 +0200

    style: add disabled styles to button component

--- components/button.tsx
+++ components/button.tsx
@@ -18,12 +18,16 @@ export default function Button({
       onClick={onClick}
       disabled={disabled}
       type={type}
-      className={classNames("border-2 transition py-1 px-4", className, {
-        "bg-primary border-primary hover:border-primary/0 hover:bg-primary/80 text-white":
-          variant === "solid",
-        "text-primary border-primary/50 hover:bg-primary hover:text-white":
-          variant === "outline",
-      })}
+      className={classNames(
+        "border-2 disabled:cursor-not-allowed transition py-1 px-4",
+        className,
+        {
+          "bg-primary disabled:bg-primary/50 disabled:border-primary/20 border-primary hover:border-primary/0 hover:bg-primary/80 text-white":
+            variant === "solid",
+          "text-primary disabled:bg-primary/30 disabled:hover:text-primary border-primary/50 hover:bg-primary hover:text-white":
+            variant === "outline",
+        },
+      )}
     >
       {children}
     </button>
