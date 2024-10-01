ee959841ae7fc64934984a8ef02cf3332495695d
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-01 17:55:48 +0200

    refactor: add actions column on orders table [admin-orders page]

--- app/obsidian/(protected)/orders/page.tsx
+++ app/obsidian/(protected)/orders/page.tsx
@@ -1,14 +1,40 @@
 "use client";
 
+import Button from "@/components/button";
 import SearchFilterInput from "@/components/input-labels/search-input-label";
-import SelectInput from "@/components/input-labels/select-input-label";
+import SelectInputLabel from "@/components/input-labels/select-input-label";
 import DatePickerInput from "@/components/inputs/date-picker-input";
-import { orderStatus } from "@/utils/constants/sort-filter-options";
+import EditOrderStatus from "@/components/modals/admin/edit-order-status";
+import Orders from "@/data/orders";
+import {
+  orderStatus,
+  rowsPerPageSelections,
+} from "@/utils/constants/sort-filter-options";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Table, TableBody, TableColumn, TableHeader } from "@nextui-org/react";
+import {
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
 import dayjs from "dayjs";
+import { useEffect, useMemo, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { z } from "zod";
+
+const rows = Orders.map((order) => ({
+  key: order.id,
+  id: order.id,
+  orderId: order.displayId,
+  date: dayjs(order.createdAt).format("DD MMM YYYY"),
+  items: order.products.length,
+  status: order.status,
+  amount: order.total,
+}));
 
 const columns = [
   {
@@ -35,6 +61,11 @@ const columns = [
     title: "Amount",
     dataIndex: "amount",
     key: "amount",
+  },
+  {
+    title: "Actions",
+    dataIndex: "actions",
+    key: "actions",
   },
 ];
 
@@ -64,10 +95,39 @@ export default function OrdersPage() {
     },
   });
 
+  const [isMounted, setIsMounted] = useState(false);
+  const [page, setPage] = useState(1);
+  const rowsWatcher = methods.watch("rows");
+  const [selectedOrder, setSelectedOrder] = useState<OrderDto | undefined>();
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const pages = Math.ceil(rows.length / rowsPerPage);
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return rows.slice(start, end);
+  }, [page, rowsPerPage]);
+
+  useEffect(() => {
+    !isMounted && setIsMounted(true);
+
+    setRowsPerPage(rowsWatcher ?? 5);
+    setPage(1);
+  }, [isMounted, rowsWatcher]);
+
   return (
     <>
       <title>Orders</title>
-      <div className="space-y-5 pt-5">
+      {selectedOrder && (
+        <EditOrderStatus
+          order={selectedOrder}
+          closeHandler={() => setSelectedOrder(undefined)}
+        />
+      )}
+      <div className="flex flex-col gap-5">
         <div className="flex flex-col items-stretch gap-5 xl:flex-row xl:items-start">
           <FormProvider {...methods}>
             <SearchFilterInput
@@ -76,7 +136,7 @@ export default function OrdersPage() {
               className="flex-1"
             />
 
-            <SelectInput
+            <SelectInputLabel
               resetInput={() => {
                 methods.setValue("status", "all");
               }}
@@ -94,7 +154,7 @@ export default function OrdersPage() {
         </div>
 
         <Table
-          className="mt-2 max-w-full flex-1 overflow-x-auto"
+          className="flex-1 overflow-x-auto"
           classNames={{ tbody: "border-b" }}
           aria-label="Orders table"
           removeWrapper
@@ -110,9 +170,53 @@ export default function OrdersPage() {
             ))}
           </TableHeader>
           <TableBody emptyContent={"You don't have any orders yet."}>
-            {[]}
+            {items.map((order) => (
+              <TableRow key={order.key}>
+                <TableCell className="py-3">{order.orderId}</TableCell>
+                <TableCell className="whitespace-nowrap py-3">
+                  {order.date}
+                </TableCell>
+                <TableCell className="py-3">{order.items}</TableCell>
+                <TableCell className="py-3">{order.status}</TableCell>
+                <TableCell className="py-3">${order.amount}</TableCell>
+                <TableCell className="py-3">
+                  <Button
+                    onClick={() =>
+                      setSelectedOrder((order) =>
+                        Orders.find((orderItem) => orderItem.id === order?.id),
+                      )
+                    }
+                    variant="outline"
+                  >
+                    <FaArrowUpRightFromSquare />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
+
+        <div className="flex max-w-full flex-col items-center justify-between gap-5 overflow-x-auto overflow-y-visible py-3 xs:overflow-x-hidden lg:flex-row">
+          <div className="flex flex-col items-center gap-3 text-neutral-500 xs:flex-row">
+            <span className="hidden sm:block">View</span>
+            <FormProvider {...methods}>
+              <SelectInputLabel
+                className="w-full sm:w-20 sm:min-w-20"
+                name="rows"
+                selectOptions={rowsPerPageSelections}
+              />
+            </FormProvider>
+            <span className="whitespace-nowrap">Orders per page</span>
+          </div>
+          <Pagination
+            isCompact
+            showControls
+            radius="none"
+            page={page}
+            total={pages}
+            onChange={(page) => setPage(page)}
+          />
+        </div>
       </div>
     </>
   );
