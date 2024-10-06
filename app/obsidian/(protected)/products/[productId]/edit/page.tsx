b22735340d3f32c8c6ada435b1ed0957ffaa3006
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-06 10:48:07 +0200

    feat: complete and validate edit product page

--- app/obsidian/(protected)/products/[productId]/edit/page.tsx
+++ app/obsidian/(protected)/products/[productId]/edit/page.tsx
@@ -1,33 +1,79 @@
 "use client";
 
+import Button from "@/components/button";
+import SelectInputLabel from "@/components/input-labels/select-input-label";
 import TextInputLabel from "@/components/input-labels/text-input-label";
+import TextAreaInputLabel from "@/components/input-labels/textarea-input-label";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { z } from "zod";
 
 const inputsSchema = z.object({
   name: z.string().min(1, "Enter name please"),
   category: z.string().min(1, "Enter category please"),
-  price: z.number().min(1),
+  price: z
+    .number({ message: "Enter valid number as price please" })
+    .min(1, "Minimum price is 1")
+    .default(0),
+  description: z.string().min(1, "Enter description please"),
 });
 
 type InputsType = z.infer<typeof inputsSchema>;
 
 export default function EditProductPage() {
-  const methods = useForm<InputsType>({ resolver: zodResolver(inputsSchema) });
-
-  // const onSubmit: SubmitHandler<InputsType> = data => data
+  const methods = useForm<InputsType>({
+    resolver: zodResolver(inputsSchema),
+    defaultValues: {
+      name: "Product name",
+      price: 5,
+      description: "This is product description",
+    },
+  });
 
   return (
     <>
       <title>Edit Product</title>
-      <form>
-        <TextInputLabel
-          title="Name"
-          placeholder="Enter product name"
-          register={methods.register("name")}
-        />
-      </form>
+      <FormProvider {...methods}>
+        <form
+          className="mx-auto flex w-full flex-col gap-5 xs:w-3/4 sm:w-full md:w-3/4 lg:w-1/2"
+          onSubmit={methods.handleSubmit((data) => data)}
+        >
+          <TextInputLabel
+            title="Name"
+            placeholder="Enter name"
+            register={methods.register("name")}
+            error={methods.formState.errors.name?.message}
+          />
+
+          <TextInputLabel
+            placeholder="Enter price"
+            register={methods.register("price", { valueAsNumber: true })}
+            type="number"
+            title="Price"
+            error={methods.formState.errors.price?.message}
+          />
+
+          <SelectInputLabel
+            hasHeader
+            hasHeaderButton={false}
+            title="Category"
+            name="category"
+            selectOptions={[
+              { key: "first", text: "First" },
+              { key: "second", text: "Second" },
+            ]}
+            error={methods.formState.errors.category?.message}
+          />
+
+          <TextAreaInputLabel
+            title="Description"
+            placeholder="Enter description"
+            register={methods.register("description")}
+            error={methods.formState.errors.description?.message}
+          />
+          <Button type="submit">Update Product</Button>
+        </form>
+      </FormProvider>
     </>
   );
 }
