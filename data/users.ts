9366955651b33bf1ce32f0a16eb616e8927c614a
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-10 15:37:46 +0200

    fix: add active property on userDto dummy data

--- data/users.ts
+++ data/users.ts
@@ -5,26 +5,31 @@ const Users: UserDto[] = [
     id: faker.string.uuid(),
     name: faker.person.fullName(),
     email: faker.internet.email().toLowerCase(),
+    active: faker.datatype.boolean(),
   },
   {
     id: faker.string.uuid(),
     name: faker.person.fullName(),
     email: faker.internet.email().toLowerCase(),
+    active: faker.datatype.boolean(),
   },
   {
     id: faker.string.uuid(),
     name: faker.person.fullName(),
     email: faker.internet.email().toLowerCase(),
+    active: faker.datatype.boolean(),
   },
   {
     id: faker.string.uuid(),
     name: faker.person.fullName(),
     email: faker.internet.email().toLowerCase(),
+    active: faker.datatype.boolean(),
   },
   {
     id: faker.string.uuid(),
     name: faker.person.fullName(),
     email: faker.internet.email().toLowerCase(),
+    active: faker.datatype.boolean(),
   },
 ];
 
