b3743652dc949ed78846fa73d1a3db7296d5f703
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-15 01:49:55 +0200

    refactor: add role property in session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -5,12 +5,14 @@ declare module "next-auth" {
   interface Session {
     user: {
       role?: "user" | "admin";
+      active?: boolean;
       token?: string;
     } & DefaultSession["user"];
   }
 
   interface User {
     role?: "user" | "admin";
+    active?: boolean;
     picture?: string;
     token?: string;
   }
@@ -19,6 +21,7 @@ declare module "next-auth" {
 declare module "next-auth/jwt" {
   interface JWT {
     role?: "user" | "admin";
+    active?: boolean;
     image?: string;
     token?: string;
   }
