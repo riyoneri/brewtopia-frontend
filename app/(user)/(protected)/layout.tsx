740bca1a037d19ef68d06c66bf74fb6d470274da
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-19 14:06:18 +0200

    feat: create deactivated component

--- app/(user)/(protected)/layout.tsx
+++ app/(user)/(protected)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import DeactivatedIllustration from "@/assets/illustrations/deactivated.illustration";
 import AuthLoading from "@/components/auth-loading";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -34,6 +35,24 @@ export default function AdminRootLayout({
     signOut({ redirect: false });
     return;
   }
+
+  if (!session?.user.active)
+    return (
+      <div className="hero-height grid place-content-center gap-2 px-5 text-center">
+        <DeactivatedIllustration className="mx-auto w-2/3 sm:w-1/2" />
+        <p className="py-5 text-xl font-medium">Hey Lionel Kaneza,</p>
+        <p>
+          We hope you&apos;re well. Your Brewtopia account is currently
+          deactivated.
+        </p>
+
+        <p>
+          If this is an error or you need help, we&apos;re here for you. Reach
+          out anytime! Cheers,
+        </p>
+        <p className="font-medium">The Brewtopia Team</p>
+      </div>
+    );
 
   return <div>{children}</div>;
 }
