1cf921cada65791f25ce76dc8d58385fce178bf0
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-20 04:24:20 +0200

    feat: connect to socket server on client side

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import Footer from "@/components/footer";
 import UserNavBar from "@/components/user/user-navbar";
+import { connectSocketServer } from "@/helpers/socket";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function UserRootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) connectSocketServer(session?.user.token, session?.user.role);
+  }, [session]);
+
   return (
     <main>
       <UserNavBar />
