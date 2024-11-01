645a2edc57f8bb660a11c879129473ee5d0beff2
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-01 11:33:10 +0200

    feat: send request of creating a category

--- app/obsidian/(protected)/products/categories/create/page.tsx
+++ app/obsidian/(protected)/products/categories/create/page.tsx
@@ -2,8 +2,12 @@
 
 import Button from "@/components/button";
 import TextInputLabel from "@/components/input-labels/text-input-label";
+import useAdminCreateCategory from "@/hooks/admin/use-admin-create-category";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { RedirectType, redirect } from "next/navigation";
+import { enqueueSnackbar } from "notistack";
+import { useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 
 const inputsSchema = z.object({
@@ -14,6 +18,12 @@ type InputsType = z.infer<typeof inputsSchema>;
 
 export default function CreateCategoryPage() {
   const {
+    createCategoryData,
+    createCategoryError,
+    createCategoryIsLoading,
+    createCategoryMutate,
+  } = useAdminCreateCategory<InputsType>();
+  const {
     register,
     formState: { errors },
     handleSubmit,
@@ -21,21 +31,42 @@ export default function CreateCategoryPage() {
     resolver: zodResolver(inputsSchema),
   });
 
+  useEffect(() => {
+    if (createCategoryData) {
+      enqueueSnackbar("New category created", { variant: "success" });
+      redirect("./", RedirectType.replace);
+    }
+  }, [createCategoryData]);
+
+  const onSubmit: SubmitHandler<InputsType> = (data) => {
+    createCategoryMutate(JSON.stringify(data));
+  };
+
   return (
     <>
       <title>Create Category</title>
       <form
         className="mx-auto flex w-full flex-col gap-5 xs:w-3/4 sm:w-full md:w-3/4 lg:w-1/2"
-        onSubmit={handleSubmit((data) => data)}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <TextInputLabel
           title="Name"
           placeholder="Enter name"
           register={register("name")}
-          error={errors.name?.message}
+          error={
+            errors.name?.message || createCategoryError?.validationErrors?.name
+          }
         />
 
-        <Button type="submit">Create</Button>
+        {createCategoryError?.message && (
+          <p className="text-center text-sm text-accent-red xs:text-base">
+            {createCategoryError.message}
+          </p>
+        )}
+
+        <Button disabled={createCategoryIsLoading} type="submit">
+          Create
+        </Button>
       </form>
     </>
   );
