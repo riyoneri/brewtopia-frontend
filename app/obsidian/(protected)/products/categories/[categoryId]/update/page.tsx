54836ad07648b894e8d0f9ac952be353c09904f2
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-06 08:56:18 +0200

    refactor: use get-single-category hook on update category page

--- app/obsidian/(protected)/products/categories/[categoryId]/update/page.tsx
+++ app/obsidian/(protected)/products/categories/[categoryId]/update/page.tsx
@@ -2,7 +2,9 @@
 
 import Button from "@/components/button";
 import TextInputLabel from "@/components/input-labels/text-input-label";
+import { useGetSingleCategory } from "@/hooks/admin/use-admin-get-single-category";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { notFound, useParams } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -17,29 +19,53 @@ export default function UpdateCategoryPage() {
     register,
     formState: { errors },
     handleSubmit,
+    setValue,
   } = useForm<InputsType>({
     resolver: zodResolver(inputsSchema),
-    defaultValues: {
-      name: "Category name",
-    },
   });
+
+  const { categoryId } = useParams<{ categoryId: string }>();
+  const {
+    getSingleCategoryData,
+    getSingleCategoryError,
+    getSingleCategoryLoading,
+  } = useGetSingleCategory(categoryId);
+
+  getSingleCategoryData && setValue("name", getSingleCategoryData.name);
+
+  if (!categoryId) notFound();
 
   return (
     <>
       <title>Update Category</title>
-      <form
-        className="mx-auto flex w-full flex-col gap-5 xs:w-3/4 sm:w-full md:w-3/4 lg:w-1/2"
-        onSubmit={handleSubmit((data) => data)}
-      >
-        <TextInputLabel
-          title="Name"
-          placeholder="Enter name"
-          register={register("name")}
-          error={errors.name?.message}
-        />
+      {getSingleCategoryLoading && (
+        <div className="flex flex-col items-center">
+          <span className="dui-loading dui-loading-spinner mx-auto"></span>
+          <p>Getting category...</p>
+        </div>
+      )}
 
-        <Button type="submit">Update</Button>
-      </form>
+      {getSingleCategoryError && (
+        <p className="text-center text-sm text-accent-red xs:text-base">
+          {getSingleCategoryError.message}
+        </p>
+      )}
+
+      {getSingleCategoryData && (
+        <form
+          className="mx-auto flex w-full flex-col gap-5 xs:w-3/4 sm:w-full md:w-3/4 lg:w-1/2"
+          onSubmit={handleSubmit((data) => data)}
+        >
+          <TextInputLabel
+            title="Name"
+            placeholder="Enter name"
+            register={register("name")}
+            error={errors.name?.message}
+          />
+
+          <Button type="submit">Update</Button>
+        </form>
+      )}
     </>
   );
 }
