384cd38de04c975b5caedb717e943b6d2f258400
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-06 08:55:07 +0200

    feat: create admin custom hook for fetching single category

---
+++ hooks/admin/use-admin-get-single-category.ts
@@ -0,0 +1,23 @@
+import { fetcher } from "@/helpers/fetcher";
+import { useQuery } from "@tanstack/react-query";
+
+export function useGetSingleCategory(categoryId: string) {
+  const { data, isLoading, error, refetch } = useQuery<
+    unknown,
+    FetcherResponse,
+    CategoryDto
+  >({
+    queryFn: () =>
+      fetcher({
+        url: `/admin/categories/${categoryId}`,
+      }),
+    queryKey: ["category", categoryId],
+  });
+
+  return {
+    getSingleCategoryData: data,
+    getSingleCategoryLoading: isLoading,
+    getSingleCategoryError: error,
+    getSingleCategoryRefetch: refetch,
+  };
+}
